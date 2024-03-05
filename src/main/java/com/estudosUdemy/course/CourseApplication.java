8e5b6ff8a091ed21234cb51ebb8a5a6d1275c5ef
José Luiz <zelfonseca@yahoo.com>
2024-03-05 19:15:23 -0300

    Projet created

---
+++ src/main/java/com/estudosUdemy/course/CourseApplication.java
@@ -0,0 +1,20 @@
+package com.estudosUdemy.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
