9089ee14e1585db9c55018f7440aa2b87b3bd9c5
José Luiz <zelfonseca@yahoo.com>
2024-03-05 20:18:34 -0300

    User entity and resource

---
+++ src/main/java/com/estudosUdemy/course/resources/UserResource.java
@@ -0,0 +1,32 @@
+package com.estudosUdemy.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudosUdemy.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	//Método=======================
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "987654321", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
