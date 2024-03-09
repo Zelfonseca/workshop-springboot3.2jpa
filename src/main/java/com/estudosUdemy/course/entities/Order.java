b4d5e5a10f4ec8ed08f940542a3e6bc59b305cd2
José Luiz <zelfonseca@yahoo.com>
2024-03-09 18:20:25 -0300

    OrderStatus enum

--- src/main/java/com/estudosUdemy/course/entities/Order.java
+++ src/main/java/com/estudosUdemy/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.estudosUdemy.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -20,31 +21,35 @@ public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	// Atributos============
+	// Atributos===================================
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd'T' HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = " client_id ")
 	private User client;
 
-	// Construtores=============
+	
+	// Construtores================================
 	public Order() {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
-	// Get And Set=================
+	// Get And Set=================================
 	public Long getId() {
 		return id;
 	}
@@ -61,6 +66,16 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public User getClient() {
 		return client;
 	}
@@ -69,7 +84,7 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	// HashCode and Equals==============
+	// HashCode and Equals==================================
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
